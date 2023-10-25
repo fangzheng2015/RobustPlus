aef01c318173bb98c20145fddcec4ea31f956f6e
funder <>
2023-10-25 10:39:50 +0800

    test

--- app/src/main/java/com/funder/robustplus/MainActivity2.java
+++ app/src/main/java/com/funder/robustplus/MainActivity2.java
@@ -1,8 +1,14 @@
 package com.funder.robustplus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.meituan.robust.PatchExecutor;
+import com.meituan.robust.patch.PatchManipulateImp;
+import com.meituan.robust.patch.RobustCallBackSample;
+import com.meituan.robust.patch.RobustModify;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity2 extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  private void runRobust() {
+    new PatchExecutor(getApplicationContext(), new PatchManipulateImp(),
+        new RobustCallBackSample()).start();
+  }
+
+  private void test() {
+    RobustModify.modify();
+    Toast.makeText(this, "hello ", Toast.LENGTH_LONG).show();
   }
 }
